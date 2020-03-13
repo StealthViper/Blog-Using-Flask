81da6c999aa1662a54880e64f6c552fcd4c22a67
Vihaan Misra <52353702+StealthViper@users.noreply.github.com>
2020-03-13 19:32:53 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '758205d833c0dd9be9a7065d2f99d21bd6a9851c4e47e8517f172b83558a17d9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+from Flask import routes
